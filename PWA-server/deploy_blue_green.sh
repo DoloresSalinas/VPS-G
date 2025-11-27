dd5e2dc7f821fd8e7c81dbc4cdd4c1dae5b50eb8
Dolores <maria2doloresjimenez@gmail.com>
2025-11-27 17:39:24 -0600

    Update deploy_blue_green.sh regex

--- PWA-server/deploy_blue_green.sh
+++ PWA-server/deploy_blue_green.sh
@@ -68,7 +68,7 @@ done
 # Cambiar NGINX al nuevo contenedor
 # ---------------------------
 echo "-> Actualizando NGINX para usar puerto $PORT..."
-sudo sed -i "s|proxy_pass http://127.0.0.1:.*;|proxy_pass http://127.0.0.1:${PORT};|" $NGINX_CONF
+sudo sed -i 's|proxy_pass http://127\.0\.0\.1:[0-9]\+;.*|proxy_pass http://127.0.0.1:'"$PORT"';|g' $NGINX_CONF
 sudo nginx -t && sudo systemctl reload nginx
 
 echo "✅ NGINX apunta ahora a $COLOR"
